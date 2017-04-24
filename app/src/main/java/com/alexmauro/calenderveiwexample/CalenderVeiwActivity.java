598e374a9f8e2f8b4d70931b57e91267dbc0e0af
Alexander Mauro <Alexander Mauro>
2017-04-23 22:10:43 -0400

    Initial commit

---
+++ app/src/main/java/com/alexmauro/calenderveiwexample/CalenderVeiwActivity.java
@@ -0,0 +1,15 @@
+package com.alexmauro.calenderveiwexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalenderVeiwActivity extends AppCompatActivity {
+
+    CalendarView calender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender_veiw);
+    }
+}
